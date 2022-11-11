eedbe7b4ac97d587e04dae13d38fd0823e38bc0c
Juan Felipe Alvarez <juanfelipe2099@gmail.com>
2022-11-10 21:57:36 -0500

    Se agrego un onclick

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafo = document.querySelector('.parrafo');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btn-calcular');
+const respuesta = document.querySelector('#respuesta1')
 
-console.log(input.value)
 
-console.log({
-    h1,
-    p,
-    parrafo,
-    pid,
-    input
-})
 
-h1.innerHTML = 'Cosita Linda <br> te amo'
-//console.log(h1.getAttribute('class'))
-//h1.setAttribute('class','rojo')
-
-h1.classList.add('Rojo')
-h1.classList.remove('verde')
-
-input.value = "Eres preciosa"
-
-const img = document.createElement('img')
-img.setAttribute('src','https://www.geekmi.news/__export/1650553082065/sites/debate/img/2022/04/21/trixie1_1.jpg_54404068.jpg')
-
-pid.append(img)
+function btnOnClick(){
+  const suma = parseInt(input1.value)+ parseInt(input2.value);
+  respuesta.innerText = suma;
+}
